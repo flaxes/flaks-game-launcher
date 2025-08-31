f70ab023b3b69c0732ed6fe6e556ac70c6bbf1d7
Jack <flaxes09@gmail.com>
2025-08-31 17:49:44 +0300

    Added metadata

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flaks Game Launcher
---
+++ types/global.d.ts
@@ -0,0 +1,7 @@
+declare interface LinkTargetInfo {
+    Name: string;
+    FullName: string;
+    Target: string;
+    Arguments: string;
+    WorkingDir: string;
+}
